3ef67915f2acda7ffd47f701a8e748dc45d83985
Kade Christlieb <kchristlieb@gmail.com>
2019-09-16 15:42:54 -0600

    Started js and added more css to go with future js code.

---
+++ assets/javascript/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+    // GLOBAL VARABLES
+
+    var timeLeft = 15;
+    var intervalId;
+    var userSelection;
+    var answered;
+    var correctAnswer = 0;
+    var incorrectAnswer = 0;
+    var currentQuestionIndex = 0;
+
+    // QUESTIONS
+
+    var questionsArray = [
+        {
+            question: "1/10: What item do you use to pick up water and lava",
+            choices: ["A Shovel", "Your Hand", "A Bucket", "You Don't"], 
+            correct: 2,
+        },
+        {
+            question: "2/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "3/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "4/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "5/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "6/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "7/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "8/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "9/10",
+            choices: [""], 
+            correct: ,
+        },
+        {
+            question: "10/10",
+            choices: [""], 
+            correct: ,
+        },
+    ]
+
+})
